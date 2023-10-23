8b81854cdb0b6340ecfaa11146f823569b404624
Anibal <anibal.flores20@yahoo.com>
2023-10-23 13:32:53 -0700

    Added style

--- Big Batch Buddy.js
+++ Big Batch Buddy.js
@@ -2,15 +2,15 @@ document.getElementById('inputValue').addEventListener('input',function(e){
     let inputValue = e.target.value;
     const Tbsp = 15;
     const tsp = 5;
-document.getElementById('fourthCupOutput').innerHTML = Math.round(Number(inputValue * Tbsp / 60));
-document.getElementById('halfCupOutput').innerHTML = Math.round(Number(inputValue * Tbsp / 118));
-document.getElementById('thirdCupOutput').innerHTML = Math.round(Number(inputValue * Tbsp / 177));
-document.getElementById('cupOutput').innerHTML = Math.round(Number(inputValue * Tbsp / 237));
+document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * Tbsp / 60));
+document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * Tbsp / 118));
+document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * Tbsp / 177));
+document.getElementById('cupOutput').innerHTML = (Number(inputValue * Tbsp / 237));
 
-document.getElementById('fourthCupOutput').innerHTML = Math.round(Number(inputValue * tsp / 60));
-document.getElementById('halfCupOutput').innerHTML = Math.round(Number(inputValue * tsp / 118));
-document.getElementById('thirdCupOutput').innerHTML = Math.round(Number(inputValue * tsp / 177));
-document.getElementById('cupOutput').innerHTML = Math.round(Number(inputValue * tsp / 237));
+document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * tsp / 60));
+document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * tsp / 118));
+document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * tsp / 177));
+document.getElementById('cupOutput').innerHTML = (Number(inputValue * tsp / 237));
 });
        
 
