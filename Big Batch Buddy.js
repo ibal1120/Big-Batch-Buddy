1527824507f9f3438183844b11f7c4df061dc16e
Anibal <anibal.flores20@yahoo.com>
2023-10-26 16:08:18 -0700

    Edit on the CSS file

--- Big Batch Buddy.js
+++ Big Batch Buddy.js
@@ -2,15 +2,15 @@ document.getElementById('inputValue').addEventListener('input',function(e){
     let inputValue = e.target.value;
     const Tbsp = 15;
     const tsp = 5;
-document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * Tbsp / 60));
-document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * Tbsp / 118));
-document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * Tbsp / 177));
-document.getElementById('cupOutput').innerHTML = (Number(inputValue * Tbsp / 237));
+document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * Tbsp / 60)).toFixed(4);
+document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * Tbsp / 118)).toFixed(4);
+document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * Tbsp / 177)).toFixed(4);
+document.getElementById('cupOutput').innerHTML = (Number(inputValue * Tbsp / 237)).toFixed(4);
 
-document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * tsp / 60));
-document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * tsp / 118));
-document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * tsp / 177));
-document.getElementById('cupOutput').innerHTML = (Number(inputValue * tsp / 237));
+document.getElementById('fourthCupOutput').innerHTML = (Number(inputValue * tsp / 60)).toFixed(4);
+document.getElementById('halfCupOutput').innerHTML = (Number(inputValue * tsp / 118)).toFixed(4);
+document.getElementById('thirdCupOutput').innerHTML = (Number(inputValue * tsp / 177)).toFixed(4);
+document.getElementById('cupOutput').innerHTML = (Number(inputValue * tsp / 237)).toFixed(4);
 });
        
 
